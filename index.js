0e2a93cf3f14405d2905a9585e49e70a3de1f537
Joao Marcos <joaotorresmarques1@gmail.com>
2020-05-01 10:33:06 -0300

    Add Relatorio, Readme, deletar

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ app.get("/cadastro",function(req,res){
     res.render('cadastro');
 })
 
+app.get("/relatorio",function(req,res){
+    Usuario.findAll().then(function(x){
+        res.render('relatorio',{user: x});
+    })
+})
+
+app.get('/deletar/:id',function(req,res){
+    Usuario.destroy({where: {'id': req.params.id}}).then(function(){
+        res.send('Deletado com sucesso!')
+    }).catch(function(erro){
+        res.send('usuario nao existe')
+    })
+})
+
 app.post("/add",function(req,res){
     Usuario.create({
         login: req.body.login,
